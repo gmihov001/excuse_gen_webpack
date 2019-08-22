a32a5c70167a002b57e747ee029ac549c653a2f7
gmihov001 <gmihov001@gmail.com>
2019-08-22 00:16:45 +0000

    Initial

--- src/js/index.js
+++ src/js/index.js
@@ -8,4 +8,24 @@ import "../style/index.scss";
 
 window.onload = function() {
   console.log("Hello Rigo from the console!");
+
+  let who = ["the dog", "my grandma", "his turtle", "my bird"];
+  let what = ["ate", "pooped", "crushed", "broke"];
+  let when = [
+    "before the class",
+    "too late",
+    "before I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let anything =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)];
+
+  let message = document.querySelector("#excuse");
+  message.innerHTML = anything;
 };
